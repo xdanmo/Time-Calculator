0f5efca16c64eaa78cec46eaf64efd96453eb9d4
XDANMO <101450888+xdanmo@users.noreply.github.com>
2024-12-07 18:01:10 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import math
+
+n = int(input('Enter the no. of value you want to Add: '))
+
+a = []
+b = []
+c = []
+
+for i in range(n):
+    x = float(input('\nEnter the value of Hour: '))*3600
+    y = float(input('Enter the value of Minutes: '))*60
+    z = float(input('Enter the value of Second: '))
+    a.append(x)
+    b.append(y)
+    c.append(z)
+    
+ts = sum(a + b + c)
+print(ts)
+
+h = math.floor(ts/3600)
+rt = ts % 3600
+m = math.floor(rt/60)
+s = rt%60
+
+print('Hours: ',h)
+print('Minutes: ',m)
+print('Seconds: ',s)
